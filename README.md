cc57ea98da9950e53777233dbec8187705e64ecd
Cesar Redondo <credondo@paradigmadigital.com>
2017-03-23 18:36:24 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# angular-components-cli
+
+Nodejs CLI to generate all needed files in angular when creating components with the best practices
+
+### Global install
+
+```
+npm install angular-components-cli -g
+```
+
+### how to use it
+
+```
+angularjs component [component-name]
+```
+
+### Example
+
+```
+angularjs component my-component
+
+// Generates the files with needed code:
+
+my-component.component.js
+my-component.controller.js
+my-component.spec.js
+my-component.html
+```
---
+++ angularjs.js
@@ -0,0 +1,111 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const fs = require('fs');
+
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
+program
+    .version('0.0.7');
+
+program.command('component')
+    .description('creates the folder and file for an angularJS component')
+    // .option("-n, --name", "Name of the element or component")
+    .action(function(name) {
+        var arrayName = name.toLowerCase().split('-');
+
+        let fileComponent = name.toLowerCase() + '.component.js';
+        let fileController = name.toLowerCase() + '.controller.js';
+        let fileTest = name.toLowerCase() + '.spec.js';
+        let fileHtml = name.toLowerCase() + '.html';
+
+        let nameComponent = '';
+        let nameController = '';
+
+        arrayName.forEach(function (item, key) {
+            if (key === 0) {
+                nameComponent = item;
+            } else {
+                nameComponent += item.capitalize();
+            }
+            nameController += item.capitalize();
+        });
+        nameController += 'Controller';
+
+        const sourceComponent = "(function () {\n" +
+            "  'use strict';\n\n" +
+
+            "  var " + nameComponent + " = {\n" +
+            "    templateUrl: '" + __dirname + "/" + fileHtml +"',\n" +
+            "    bindings: {\n" +
+            "    },\n" +
+            "    controller: '" + nameController +"'\n" +
+            "  };\n\n" +
+
+            "  angular\n" +
+            "    .module('app')\n" +
+            "    .component('" + nameComponent + "', " + nameComponent + ");\n" +
+            "})();\n";
+
+        const sourceController = "(function () {\n" +
+            "  'use strict';\n\n" +
+
+            "  angular\n" +
+            "    .module('app')\n" +
+            "    .controller('" + nameController + "', " + nameController + ");\n\n" +
+
+            "  /** @ngInject */\n" +
+            "  function " + nameController + "() {\n" +
+            "    var vm = this;\n\n" +
+
+            "    vm.$onInit = function () {\n" +
+            "    };\n" +
+            "  }\n" +
+            "})();\n";
+
+        const sourceTest = `describe('component: ${nameComponent}', function () {
+  var $componentController;
+
+  beforeEach(module('app'));
+  beforeEach(angular.mock.inject(function (_$componentController_) {
+    $componentController = _$componentController_;
+  }));
+
+  // Here we are passing actual bindings to the component
+  var bindings = {
+  };
+
+  it('should expose a "${nameComponent}" object', function () {
+    var ctrl = $componentController('${nameComponent}', null, bindings);
+
+    expect(ctrl).toBeDefined();
+    
+    ctrl.$onInit();
+  });
+});
+`;
+
+        const sourceHtml = "<h1>" + nameComponent + "</h1>";
+
+        fs.writeFile(fileComponent, sourceComponent, function(err) {
+            if(err) console.log(err.red);
+        });
+
+        fs.writeFile(fileController, sourceController, function(err) {
+            if(err) console.log(err.red);
+        });
+
+        fs.writeFile(fileTest, sourceTest, function(err) {
+            if(err) console.log(err.red);
+        });
+
+        fs.writeFile(fileHtml, sourceHtml, function(err) {
+            if(err) console.log(err.red);
+        });
+    });
+
+program.parse(process.argv);
+
+if (!program.args.length) program.help();
